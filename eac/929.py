6f17ec0c399e966e63e7c328646a8733122b14be
kjh1997 <aam41@naver.com>
2022-01-28 13:12:56 +0900

    baekjun start 0128

--- eac/929.py
+++ eac/929.py
@@ -1,22 +1,21 @@
 T = int(input())
 for test_case in range(1, T + 1):
     a = int(input())
-    data, chk, mul, base =  [9,8,7,6,5,4,3,2,1], 0, [], a
+    chk, mul, base =  0, [], a
     if a <= 10: 
         result = "Yes"
     else:    
-        for i in data:
+        for i in range(9,0,-1):
             if chk == 1: break
             if len(mul) != 1:
-                for j in data:
+                for j in range(9,0,-1):
                     if a // j == a / j and j != 1:
                         mul.append(j)
                         a /= j
                         break
                     elif j == 1:
                         chk=1
-                        break 
-                    
+                        break                    
             if len(mul) == 0:chk =1;  break; 
             if chk == 1: break
             if a // i == a / i and i != 1: mul.append(i); break
