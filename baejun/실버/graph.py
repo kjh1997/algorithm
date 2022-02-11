9af0c0e709214a15994143efa5ad6ebfb0b2cecf
kjh1997 <aam41@naver.com>
2022-02-11 23:41:39 +0900

    dp

---
+++ baejun/실버/graph.py
@@ -0,0 +1,20 @@
+from collections import deque
+n,m = map(int,input().split())
+data = [[] for i in range(n+1)]
+print(data)
+for i in range(m):
+    x,y=map(int,input().split())
+    data[x].append(y); data[y].append(x)
+    print(data)
+
+def bfs(start):
+    stack = deque()
+    stack.append(start)
+    while stack:
+        idx=stack.pop()
+        stack.extend(data[idx])
+        data.pop(idx)
+cnt = 0
+while data:
+    x = data.pop(0)
+    bfs(x)
