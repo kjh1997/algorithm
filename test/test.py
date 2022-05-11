93d63dd638e241bfac5cbdb1be2a5d75e577607e
kjh1997 <aam41@naver.com>
2022-05-11 09:23:13 +0900

    test

--- test/test.py
+++ test/test.py
@@ -1,2 +1,13 @@
-a = [[0]*10 for i in range(3)]
-print(a)
+a = set([1,2,3,4])
+b = set([3,4,5,6])
+print(a.intersection(b))
+
+def sum_ab(a,b):
+    for i in range(5):
+        yield (a,b)
+        print("?!@#")
+    
+
+for i in range(1,10):
+    for j in sum_ab(i,i-1):
+        print(j)
