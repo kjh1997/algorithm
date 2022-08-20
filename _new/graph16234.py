7e1872759366ee5b6e051545614134139b6be688
kjh <aam41@naver.com>
2022-08-20 22:33:21 +0900

    flued fill algorithm

--- _new/graph16234.py
+++ _new/graph16234.py
@@ -5,13 +5,16 @@ n,l,r = map(int,input().split())
 
 data  = [list(map(int,input().split())) for _ in range(n)]
 from collections import deque
-visit = []
+
 re = 0
 def bfs(x,y):
+    visit = []
+    visit.append((w,h))
     q = deque()
     q.append((w,h))
     result = 0
-    
+    sum_board=0
+    sum_board += data[h][w]
     while q:
         cx,cy = q.popleft()
 
@@ -22,14 +25,17 @@ def bfs(x,y):
                 if  l <= abs(data[ny][nx]-data[cy][cx]) <= r :
                     visit.append((nx,ny))
                     q.append((nx,ny))
+                    sum_board += data[ny][nx]
                     result += 1
+    sum_board = sum_board//len(visit)
+    for nx,ny in visit:
+        data[ny][nx]=sum_board
     if result != 0: return 1
     else: return 0
 for w in range(n):
     for h in range(n):
-        if (w,h) not in visit:
-            visit.append((w,h))
-            if 0 != bfs(w,h): re+=1
-            print(visit)
+        if 0 != bfs(w,h): re+=1
+        print(data)
+            
 print(re)
 
